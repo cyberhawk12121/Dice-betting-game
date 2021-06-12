6c2ddb8aec6f587b3187183c2c4b5c13d2d4ec3e
cyberhawk12121 <hellosam1010@gmail.com>
2021-06-13 02:32:47 +0530

    UI update: Basic alert box when game is completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import Web3 from 'web3';
+import Navbar from "./Navbar.js"
+
 
 import Dice from "../abis/Dice.json";
 
@@ -10,6 +12,7 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state={
+      bet: null,
       balance: null,
       account: null,
       network:null,
@@ -17,9 +20,10 @@ class App extends Component {
       contractAddress: null,
       event: null,
       maxBet:0,
-      minBet:0,
+      // minBet:0,
       loading:false,
       contract: null,
+      amount:null
     }
 
     this.onChange= this.onChange.bind();
@@ -27,18 +31,33 @@ class App extends Component {
     this.makeBet= this.makeBet.bind();
   }
 
-  async ComponentWillMount(){
+  async componentWillMount(){
     await this.loadWeb3();
     await this.chainData();
   }
 
   async chainData(){
+    let accounts, balance, maxBet, minBet;
+
     const netid= await web3.eth.net.getId();
-    const dice= Dice.networks[netId];
+    const dice= Dice.networks[netid];
     if(dice){
       // Getting the smart contract using the address and ABI (ABI: Is the binary formatted metadata of the contract)
       const diceGame= new web3.eth.Contract(Dice.abi, dice.address);
       this.setState({contract: diceGame});
+    }
+
+    accounts= await web3.eth.getAccounts();
+
+    if(typeof accounts[0]!=='undefined' && accounts[0]!== null) // !== means not equal value or type
+    {
+      balance= await web3.eth.getBalance(accounts[0]);
+      maxBet= await web3.eth.getBalance(dice.address)
+      this.setState({
+        account: accounts[0],
+        balance: balance,
+        maxBet:maxBet,
+      })
     }
   }
 
@@ -57,6 +76,30 @@ class App extends Component {
     else{
       alert("Install metamask!");
     }
+    window.ethereum.autoRefreshOnNetworkChange = false;
+  }
+
+  onChange=({target})=>{
+    this.setState({[target.name]: target.value});
+  };
+
+  async onSubmit(e){
+    e.preventDefault();
+    if(typeof this.state.account !=='undefined' && this.state.account !==null){
+      var randomSeed = Math.floor(Math.random() * Math.floor(1e9))
+    }
+
+    this.state.contract.methods.bet(this.state.bet, randomSeed).send({from:this.state.account, value:this.state.amount}).on('transactionHash', (hash)=>{
+      this.state.contract.events.Result({}, async (error, event)=>{
+        const result= event.returnValues.winAmount;
+        if(result==='0'){
+          window.alert("You lose!");
+        }else{
+          window.alert("WIN");
+        }
+      })
+    })
+    
   }
 
   render() {
@@ -64,8 +107,11 @@ class App extends Component {
       <div>
         <h1>Hello</h1>
         <Navbar account={this.state.account} />
-        <form>
-          <input type="number" />
+        <form onSubmit={this.onSubmit}>
+          Number to Bet<input type="number" name="bet" value={this.state.bet} onChange={this.handleChange} />
+          <br/>
+          Amount you're betting<input type="number" name="amount" value={this.state.amount} onChange={this.handleChange} />
+          <input type="submit"  value="Roll it!" className="btn btn-outline-danger" />
         </form>      
       </div>
     );
--- truffle.js
+++ truffle.js
@@ -1,5 +1,5 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
-const MNEMONIC = '0xA491C397e86AB73D46c703B76B74e7d431656853';
+const MNEMONIC = 'Your-mnemonic';
 
 module.exports = {
   networks: {
@@ -10,7 +10,7 @@ module.exports = {
     },
     ropsten: {
       provider: function() {
-        return new HDWalletProvider(MNEMONIC, "https://ropsten.infura.io/v3/84e5ee7d74cb499d9d0f98af91ee62e1")
+        return new HDWalletProvider(MNEMONIC, "https://ropsten.infura.io/v3/<Project-ID>")
       },
       network_id: 3,
       gas: 4000000      //make sure this gas allocation isn't over 4M, which is the max
